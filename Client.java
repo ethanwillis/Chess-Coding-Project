fb9765ff2af86fdb91c6f73f978267bd0d8a3e7c
Marshal Hayes <mdhayes.07@live.com>
2016-12-14 02:48:50 -0600

    Added play() method to Client

--- Client.java
+++ Client.java
@@ -1,4 +1,54 @@
+import java.util.Scanner;
+
 public class Client {
+    private static Board board = new Board();
+    
+    // play() performs the necessary tasks to let the user play Chess 
+    public static void play() {
+        double Carlsen = 6.3;
+        double Karjakin = 6.1;
+        
+        Scanner s = new Scanner(System.in);
+        
+        board.setupBoard();
+        
+        do {
+            System.out.println("What would you like to do?");
+            System.out.println("1 Add a piece, 2 Move a piece, 3 Display the Board, 4 Clear the Board, 5 Setup starting position, 6 Terminate");
+            int input = s.nextInt();
+            
+            if (input == 1) {
+                board.addPiece();
+                board.printBoard();
+            }
+            else if (input == 2) {
+                // board.movePiece();
+            }
+            else if (input == 3) {
+                System.out.println("Who's perspective? (White or Black)");
+                String color = s.next();
+                if (color.equalsIgnoreCase("White")) {
+                    board.printBoard();
+                }
+                else if (color.equalsIgnoreCase("Black")) {
+                    board.printFlippedBoard();
+                }
+            }
+            else if (input == 4) {
+                board.clearBoard();
+                board.printBoard();
+            }
+            else if (input == 5) {
+                board.setupBoard();
+                board.printBoard();
+            }
+            else if (input == 6) {
+                return;
+            }
+        }
+        while (Carlsen > Karjakin);
+    }
+    
     public static void main(String[] args) {
         System.out.println("TEXT-BASED CHESS by Marshal Hayes");
         System.out.println("------------------------------------------------------------------");
@@ -7,10 +57,12 @@ public class Client {
         System.out.println();
         System.out.println("1. White's pieces are uppercase, and Black's pieces are lowercase.");
         System.out.println("2. Currently, there is no way to capture a piece (i.e. move a piece onto an occupied square)");
-        System.out.println();
-
-        Board board = new Board();
-        board.setupBoard(); 
+        
         board.printBoard();
+        play();
+        
+        System.out.println();
+        System.out.println("Thanks for playing!");
+        System.out.println("If you enjoyed this text-based Chess program, please share it!");
     }
 }
